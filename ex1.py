e6c61f41d50187e070c62e2c057d1b754e6c63c9
GHADY NAZHA <ghadynazha25@gmail.com>
2025-02-13 10:56:19 +0200

    first commit

---
+++ ex1.py
@@ -0,0 +1,24 @@
+#age = int(input("Enter your age: "))
+
+#if age < 5:
+ #   price = 0
+#elif age <= 10:
+ #   price = 5
+#elif age <= 30:
+ #   price = 10
+#else:
+ #   price = 7
+
+#print(f"The ticket price is ${price}.")
+
+
+
+
+num = int(input("Enter a number: "))
+
+
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
